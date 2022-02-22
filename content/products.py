020deede7cdfad38162fe58e2c5ba0e90bd3103a
Tristan Chauvin <tristanchauvin@gmail.com>
2022-02-22 00:40:05 -0500

    Staging

--- content/products.py
+++ content/products.py
@@ -110,13 +110,97 @@ def getMycoNetBuilds() -> dict:
                 "Empty pressure cooker and let the jars cool down in a relatively clean place (Inside your stove while it is off)",
                 "Procure liquid mycelium syringe or spore syringe from bigfungus.ca or other source"
             ],
-            "website": "bigfungus",
+            "website": "community",
             "item_type": "Tek (Technique)",
             "price":"~ 250 $",
             "img_href": "teks/pftek.png",
             "shortDesc": "Psilocybe fanaticus technique",
             "level": "Beginner"
-        }
+        },
+        
+        "Agar Plates" : {
+            "affiliateItems": [
+
+            ],
+            "desc": "This procedure will give you a step by step on how to make your own petri dishes to isolate your mycelium",
+            "steps": [
+                "Dice 200g of potatoes",
+                "Prepare a pot with 1L of water and bring to a boil",
+                "After 15 mintues, remove the potatoes with a slotted spoon and put the potato water aside",
+                "Mix the 1L of potato water with 20g of AGAR and 20g of DEXTROSE(substitute with table sugar)",
+                "Mix thorougly and pour the mixture into 3 small MASON JARS. Put lid on but DO NOT close lid tightly as to not risk breaking the jar.",
+                "Set the agar filled mason jars in your AUTOCLAVE and sterilise for 1 hour. It is useful to use the extra autoclave space to sterilise your SCALPEL and RAZOR BLADE",
+                "Prepare PETRI DISHES by disinfecting them in a BLEACH solution for 20 minutes. The recipe for the bleach solution is as follows [1L WATER : 70mL BLEACH] this will give a 3000 ppm solution",
+                "After 20 minutes, set your petri dishes down in front of your flow hood and let them dry",
+                "While waiting for your petri dishes to dry, wait for the autoclave sterilising cycle to finish and remove your agar jars and scalpel and put them in fron of your flow hood as well",
+                "While remaining in front of your flow hood, carefully pour a small amount of potato agar into the bottom of each dish: there should be enough agar for 25 petri dishes",
+                "Assemble each dish with a matching top and let cool a bit longer to let the potato agar firm up in the dish, you are now ready to isolate your mycelium",
+            ],
+            "website": "bigfungus",
+            "item_type": "Trusted recipe",
+            "price":"~ 30 $",
+            "img_href": "teks/plates.png",
+            "shortDesc": "Big Fungus approved procedure",
+            "level": "Advanced"
+        },
+        "Tissue Culture" : {
+            "affiliateItems": [
+
+            ],
+            "desc": "This procedure will give you a step by step on how to isolate your chosen mycelium on an empty petri dish",
+            "steps": [
+                "Note: it is recommened to wear a MASK and NITRILE GLOVES for this procedure to minimise bacterial contamination",
+                "Find a suitable mushroom sample, it can be from a trusted market, grocery store, or even foraged from the forest (Make sure you know what you're getting!!)",
+                "Clean the mushroom sample gently with running water to remove debris and spray lightly with ISOPROPYL ALCOHOL",
+                "Wait for the sample to dry a bit while you compose yourself, take this moment to visualize the mushroom growing",
+                "Using the Razor blade, slice the top of the mushroom down the center going a few centimeters deep, then grab both side of the mushroom to split it even down the center",
+                "The reason for doing it this way is to avoid smearing surface bacteria into the center of the mushroom where you will take your sample",
+                "Without touching the center of the mushroom you just exposed, take your sterilised scalpel and take a small shallow sample from the middle of the mushroom",
+                "This sample should be taken where the cap meets the stem of the mushroom, this is where the mycelium will be most active",
+                "Put a small section of the sampple into each petri dish. The more you make, the more chance you have of getting a petri with no contamination",
+                "You will now wait 2-3 weeks for the samples to cover your agar plate",
+                "Some will have bacterial or fungal contamination (See our GUIDE TO RECOGNIZING CONTAMINATION)",
+                "The goal is to isolate the nicest parts of the mycelium you have grown to clean plates in order to get a nice stong and clean sample",
+                "Make sure to clean the outside of original place carefully before opening and cutting clean samples of the mycelium",
+                "If a petri dish is heavily contaminated with mold, discard it since mold spore can be invisible to the naked eye and make it harder to isolate clean samples",
+                "Repeat this procedure until you have aquired a petri dish growing only the desired mycelium",
+            ],
+            "website": "bigfungus",
+            "item_type": "Trusted recipe",
+            "price":"~ 30 $",
+            "img_href": "teks/tissueCulture.png",
+            "shortDesc": "Big Fungus approved procedure",
+            "level": "Advanced"
+        },
+        "Grain Spawn" : {
+            "affiliateItems": [
+
+            ],
+            "desc": "This procedure will give you a step by step on how to prepare your grain spawn to ready it for your chosen mycelium",
+            "steps": [
+                "Place 1kg of dry rye grain into a large pot with 4L of tap water",
+                "Bring to a boil and let simmer for 15-20 minutes while stirring constantly to avoid sticking, a RICE COOKER helps greatly with this step",
+                "The grain is ready when it is almost fully hydrated or when 2-5 percent of the grains have burst",
+                "Once the grain is sufficiently cooked, strain the grain to eliminate as much water as possible and spread out over some paper towel to dry excess moisture",
+                "Wait 10-15 minutes for the grain to cool and for the surface moisture to evaporate from the grain",
+                "Collect and use the cooked grain to fill 1-2 mushroom UNICORN BAGS",
+                "Fold the tops of the bags down to prevent them from touching the sides of the autoclave while still allowing air transfer between the inside and outisde of the bag (to prevent rupture)",
+                "Autoclave the grain spawn for 1 hour at 121 degrees Celsius",
+                "Once the sterilisation cycle is finished, place grain spawn bag in front of your flow hood (dont forget to turn the flow hood ON)",
+                "Put on gloves and mask and disinfect the outside of your isolated mycelium dish and your scalpel (if it is not sterilised already)",
+                "Before innoculating your grain, make sure it has cooled to room temperature as to not kill the mycelium",
+                "Using the scalpel, gently cut the agar in the petri dish into small pieces and put half into each bag",
+                "While remaining in front of your flow hood, seal the bag using a heat or IMPULSE SEALER, or simply tie the top of the bag tightly with a few elastics",
+                "Gently stir the bag around and place in a 20-22 degree C environment for 4-6 weeks",
+                "When the mycelium has taken over 1/3 to 1/2 of the bag, break it up and shake very gently to accelerate the full innoculation of the bag",
+            ],
+            "website": "bigfungus",
+            "item_type": "Trusted recipe",
+            "price":"~ 15 $",
+            "img_href": "teks/grainSpawn.png",
+            "shortDesc": "Big Fungus approved procedure",
+            "level": "Advanced"
+        },
         
     }
     return teks
