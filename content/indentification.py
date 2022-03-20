c2f78e04b4562fa451b04bcafca52718b87b6fb0
Tristan Chauvin <tristanchauvin@gmail.com>
2022-03-19 20:54:06 -0400

    Added to ids

--- content/indentification.py
+++ content/indentification.py
@@ -36,7 +36,6 @@ def getMycoNetIdentification() -> dict:
                 ("amanitas","amanitas5"),
                 ("agarici, amanitas, and Sharp-Scaled Lepiota","agarici, amanitas, and Sharp-Scaled Lepiota"),
                 ("agarici and lepiotas","agarici and lepiotas"),
-                ("stropharias, pholiotas, and Slimy-sheated Waxy Cap", "stropharias, pholiotas, and Slimy-sheated Waxy Cap"),
             ],
             "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
             "steps": [
@@ -52,10 +51,53 @@ def getMycoNetIdentification() -> dict:
             "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
         },
         
+        "Veiled Mushrooms with Attached Gills" : {
+            "shapes": [
+                ("pholiotas and Scaly Fiber Head", "pholiotas and Scaly Fiber Head"),
+                ("stropharias, agrocybes, and others","stropharias, agrocybes, and others"),
+                ("cystodermas, armillarias, catathelasmas, and others","cystodermas, armillarias, catathelasmas, and others"),
+                ("stropharias, pholiotas, and Slimy-sheated Waxy Cap", "stropharias, pholiotas, and Slimy-sheated Waxy Cap"),
+                ("gomphidii and chroogomphi","gomphidii and chroogomphi"),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "Veiled Mushrooms with Attached Gills",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/Veiled Mushrooms with Attached Gills.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "Mushrooms with Free Gills" : {
+            "shapes": [
+                ("plutei","plutei")
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "Mushrooms with Free Gills",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/Mushrooms with Free Gills.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
         "Mushrooms with Attached Gills" : {
             "shapes": [
                 ("melanoleucas, entolomas, and Rooted Oudemansiella","melanoleucas, entolomas, and Rooted Oudemansiella"),
-                
+                ("waxy caps, russulas, laccarias, trichs, mops, and others","waxy caps, russulas, laccarias, trichs, mops, and others"),
+                ("clitocybes, laccarias, and allies; leucopaxes and allies; waxy caps, russulas, entolomas, chanterelles, boletes, Jack O'Lantern, False Chanterelle, and others","clitocybes, laccarias, and allies; leucopaxes and allies; waxy caps, russulas, entolomas, chanterelles, boletes, Jack O'Lantern, False Chanterelle, and others"),
+                ("lactarii","lactarii"),
+                ("fiber heads and trocholomas","fiber heads and trocholomas"),
+                ("cortinarii","cortinarii"),
+                ("Lobster Mushrom","Lobster Mushrom"),
             ],
             "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
             "steps": [
@@ -69,6 +111,107 @@ def getMycoNetIdentification() -> dict:
             "img_href": "identification/Mushrooms with Attached Gills.png",
             "shortDesc": "Typical Shapes",
             "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "Boletes and Others" : {
+            "shapes": [
+                ("boletes and Golden Hypomyces","boletes and Golden Hypomyces"),
+                ("Pinecone Tooth","Pinecone Tooth"),
+                ("polypores","polypores"),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "Boletes and Others",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/Boletes and Others.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "chanterelles and Other Vase-shaped Mushrooms" : {
+            "shapes": [
+                ("chanterelles, lentinelli, Jack O'Lantern, and others","chanterelles, lentinelli, Jack O'Lantern, and others"),
+                ("chanterelles, thelephores, Apricot Jelly, and others","chanterelles, thelephores, Apricot Jelly, and others"),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "chanterelles and Other Vase-shaped Mushrooms",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/chanterelles and Other Vase-shaped Mushrooms.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "Stalked Polypores, Tooth Fungi, and Others" : {
+            "shapes": [
+                ("polypores and Yellow-green Hypomyces","polypores and Yellow-green Hypomyces"),
+                ("polypores, Ash-tree Bolete, and Black Tooth","polypores, Ash-tree Bolete, and Black Tooth"),
+                ("tooth fungi and Jelly Tooth","tooth fungi and Jelly Tooth"),
+                ("polypores, tooth fungi, and Crimped Gill","polypores, tooth fungi, and Crimped Gill"),
+                ("Thick-maze Oak Polypore","Thick-maze Oak Polypore"),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "Stalked Polypores, Tooth Fungi, and Others",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/Stalked Polypores, Tooth Fungi, and Others.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "Polypores and Other Shelflike Mushrooms" : {
+            "shapes": [
+                ("polypores, parchment fungi, and Silver Leaf Fungus","polypores, parchment fungi, and Silver Leaf Fungus"),
+                ("oyster mushrooms and Jack O'Lantern","oyster mushrooms and Jack O'Lantern"),
+                ("oyster mushrooms, creps, panelli, lentinelli, and others","oyster mushrooms, creps, panelli, lentinelli, and others"),
+                ("polypores, tooth fungi, Coral-pink Merulius, and False Turkey-tail","polypores, tooth fungi, Coral-pink Merulius, and False Turkey-tail"),
+                ("polypores","polypores"),
+                ("tooth fungi, and others","tooth fungi, and others"),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "Polypores and Other Shelflike Mushrooms",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/Polypores and Other Shelflike Mushrooms.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
+        },
+        "" : {
+            "shapes": [
+                ("",""),
+                ("",""),
+                ("",""),
+            ],
+            "desc": "From the National Audubon Society Field Guide to North America Mushrooms",
+            "steps": [
+
+            ],
+            
+            "website": "audubon",
+            "item_type": "",
+            "item_title": "Typical Shape",
+
+            "img_href": "identification/.png",
+            "shortDesc": "Typical Shapes",
+            "division": "<a href='https://en.wikipedia.org/wiki/Basidiomycota'>Basidiomycota</a>"
         }
+        
+        
     }
     return identification
