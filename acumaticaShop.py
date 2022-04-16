3289f25601ab4943a19b16c501abfe4a27c5dee9
Tristan Chauvin <tristanchauvin@gmail.com>
2022-04-16 00:30:49 -0400

    staging

--- acumaticaShop.py
+++ acumaticaShop.py
@@ -8,23 +8,44 @@ from grpc import StatusCode
 import requests
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+import mysql.connector
+from mysql.connector import Error
 import os
 # This is your test secret API key.
 # print(os.environ)
 import pyodata
 import requests
 import json
+import shutil
 
 class AcumaticaOdata():
     def __init__(self) -> None:
         self.COMPANY = 'Company'
+        self.DBENDPOINT = os.environ["DBENDPOINT"]
         self.BASEENDPT = os.environ["ACUMATICAENDPT"]
+        self.DBPASS = os.environ["DBPASS"]
         self.SERVICE_URL = f'{self.BASEENDPT}/odatav4/{self.COMPANY}/'
         self.GETFILE = f"{self.BASEENDPT}/Frames/GetFile.ashx?fileID="
-    
-    def getImageFromFileId(self):
+        try:
+            self.connection = mysql.connector.connect(host=self.DBENDPOINT,
+                                                database='acumaticadb',
+                                                user=self.DBPASS ,
+                                                password=self.DBPASS)
+            if self.connection.is_connected():
+                db_Info = self.connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        self.getImageFromDatabase()
         
-        pass
+    def getImageFromDatabase(self, fileId='b33c365a-0651-4889-af52-6d08249af45d'):
+        mycursor = self.connection.cursor()
+        mycursor.execute(f"SELECT * FROM uploadfilerevision where FileID = '{fileId}'")
+        myresult = mycursor.fetchone()
+        print(myresult[3])
+        #Save Image
+        with open("./static/fetchedImages/test.jpg", "wb") as fh:
+            fh.write(myresult[3])
     
     def get(self, endpt= "CSAnswers"):
         r=requests.get(self.SERVICE_URL+endpt, auth=('admin', os.environ["ACUMATICAPWD"]), verify=False)
