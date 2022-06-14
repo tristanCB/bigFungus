a584da2689bc5d50843b02537b47bc55a3540b70
Tristan Chauvin <tristanchauvin@gmail.com>
2022-06-14 16:57:13 -0400

    Cleanup for release

--- bigFungusWeb.py
+++ bigFungusWeb.py
@@ -1,63 +1,17 @@
-# save this as app.py
-from binascii import Incomplete
-from operator import index
-from re import template
 from flask import Flask, redirect, request, send_from_directory
-from flask import render_template, url_for, session, stream_with_context
-from flask import Response
-# from flask.logging import 
-from time import sleep
+from flask import render_template
 import datetime
-# Local modules
-# import content
-from flask_sqlalchemy import SQLAlchemy, Model, BaseQuery
-from sqlalchemy.orm.attributes import flag_modified
-
-import typing
-import os
-import glob
-# Payments https://stripe.com/docs/checkout/quickstart
-import stripe
+from time import sleep
+from flask import Response
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
-# DB interface
+
 from content.products import getProducts
 from content.techniques import getMycoNetBuilds
 from content.indentification import getMycoNetIdentification
-from content.shipping import getShippingOptionsStripe
 from content.seo import getMeta, getPages
 
-from acumaticaShop import AcumaticaOdata
-acumaticaProducts = []
-try:
-    acumaticaProducts = AcumaticaOdata().getItemWithAttributes()
-except Exception as e:
-    print(f'{e}: Error retrieving products from acumatica')
-    
-# import logging 
-
-# This is your test secret API key.
-stripe.api_key = os.environ["stripeApiKey"]
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{os.environ["DBPASS"]}:{os.environ["DBPASS"]}@{os.environ["DBENDPOINT"]}/big_fungus_db'
-db = SQLAlchemy(app)
-db.create_all()
-
-import model
-# output_file_handler = logging.FileHandler("output.log")
-# app.logger.addHandler(output_file_handler)
-
-# @app.after_request
-# def log(r):
-#     log = model.Logger(date_accessed=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ip=request.remote_addr)
-#     print(log)
-#     db.session.add(log)
-#     db.session.commit()
-#     return r
-
-keywords    = ["Steam Sterilizer", "Impulse sealer", "Mushroom bag", "Petri dish", "Isopropyl", "Hepa filter", "Humidifier"]
-distribution_ammounts = [1, 2, 3, 4, 5]
 
 # Theming
 logo_txt = 'big_fungus.png'
@@ -71,23 +25,27 @@ def static_from_root():
 
 @app.route('/shop')
 @app.route('/Shop')
-def shop():    
-    return render_template(
-        'index.html', 
-        distribution_ammounts=distribution_ammounts, 
-        products=getProducts()+acumaticaProducts,
-        css=css,
-        splash=False,
-        logo_txt=logo_txt,
-        pages=getPages(),
-        seo=getMeta()
-    )
+def shop():
+        return redirect('/Home')
+    
+@app.route('/Products')
+def productPageRedirect():
+        return redirect('/Mushroom/Products')
 
+@app.route('/Mushroom/Products')
+def productPage():
+        pageToRender = 'products.html',
+        return render_template(
+            pageToRender, 
+            logo_txt=logo_txt,
+            pages=getPages(),
+            seo=getMeta()
+        )
+
+@app.route('/', methods=['GET'])
 @app.route('/Home')
 @app.route('/About')
 def splash():
-    # print('At Home with big fungus')
-    pass
     return render_template(
         'home.html',
         logo_txt=logo_txt,
@@ -95,14 +53,7 @@ def splash():
         seo=getMeta()
     )
 
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    # rv.enable_buffering(1)
-    return rv
-
-@app.route("/time/")
+@app.route('/time/')
 def time():
     def streamer():
         while True:
@@ -110,37 +61,10 @@ def time():
             sleep(1)
     return Response(streamer())
 
-@app.route('/vote/<mycNet>/', methods=['POST'])
-@app.route('/vote/<mycNet>/<itemType>', methods=['POST'])
-def vote(mycNet = None, itemType=None):
-    if request.method == 'POST':
-        if "Equipment" in mycNet: 
-            mod = model.Autocalves
-        elif "Recipes" in mycNet: 
-            mod = model.Recipes
-        
-        if "voteDown" in request.form:
-            count =  mod.query.filter_by(id=str(request.form["voteDown"])).first()
-            if count.rating is None: 
-                count.rating = 0
-            else:
-                count.rating -= 1
-                
-        if "voteUp" in request.form:
-            count =  mod.query.filter_by(id=str(request.form["voteUp"])).first()
-            if count.rating is None: 
-                count.rating = 0
-            else:
-                count.rating += 1
-            
-        flag_modified(count, "rating")
-        db.session.merge(count)
-        db.session.flush()
-        db.session.commit()
-        return redirect(f'/Myco-NET/{mycNet}/{itemType}')
-
-@app.route('/', methods=['GET'])
-@app.route('/Guides')
+@app.route('/Grow-Guides')
+@app.route('/Grow Guides')
+@app.route('/Myco-NET')
+@app.route('/Mushroom/')
 @app.route('/Myco-NET')
 @app.route('/Myco-NET/')
 @app.route('/Mushroom')
@@ -155,38 +79,22 @@ def handle_redirect(mycNet = None, itemType = None):
 @app.route('/Mushroom/<mycNet>/<itemType>')
 def render_large_template(mycNet = None, itemType = None, items = None):
     pageToRender = 'myco-net-beta.html'
-    if (mycNet == "GrowGuides" or mycNet == "Grow-Guides"):
-        pageToRender = "mycoGrowingGuides.html"
+    if (mycNet == 'GrowGuides' or mycNet == 'Grow-Guides'):
+        pageToRender = 'mycoGrowingGuides.html'
         teks = getMycoNetBuilds()
         if itemType == None:
             items = teks
         else:
-            # The replace on items is for more human readable name, might consider just renameing products
-            items = {f"{itemType}": teks[itemType.replace('-', ' ')]}
+            items = {f'{itemType}': teks[itemType.replace('-', ' ')]}
     
-    if (mycNet == "Identification"):
-        pageToRender = "mycoIdentification.html"
+    if (mycNet == 'Identification'):
+        pageToRender = 'mycoIdentification.html'
         ids = getMycoNetIdentification()
         if itemType == None:
             items = ids
         else:
-            # The replace on items is for more human readable name, might consider just renameing products
-            items = {f"{itemType}": ids[itemType.replace('-', ' ')]}
-    
-    if (mycNet == "Equipment"):
-        if itemType == None: itemType = keywords[0]
-        print(type(model.Autocalves.query))
-        
-        baseQuery : BaseQuery = model.Autocalves.query
-        items = baseQuery.filter(model.Autocalves.item_type == itemType).order_by(model.Autocalves.rating.desc()).all()
-        # items = {}
-        # for i in keywords:
-        #     items[f'{i}'] = list([clave for clave in claves if clave.item_type == i])
-        # pp.pprint(items)
-        
-    elif (mycNet == "Recipes"):
-        items = model.Recipes.query.order_by(model.Recipes.rating.desc()).all()
-        
+            items = {f'{itemType}': ids[itemType.replace('-', ' ')]}
+
     print(mycNet)
     return render_template(
             pageToRender, 
@@ -194,41 +102,15 @@ def render_large_template(mycNet = None, itemType = None, items = None):
             logo_txt=logo_txt,
             pages=getPages(),
             mycNet=mycNet, 
-            keywords=keywords, 
+            # keywords=keywords, 
             itemType=itemType,
             seo=getMeta()
         )
+    
+@app.route('/Identification/')
+def handle_redirect_identification():
+    return redirect('/Mushroom/Identification')
 
-@app.route('/add/<code>/<quantity>', methods=['POST'])
-def add_product_to_cart(code = None, quantity = None):
-    print(code, quantity)
-    return redirect(url_for('shop', page="shop"))
-
-# Should add a thank you for ordering page, your order is being prepared when payment confirmed
-YOUR_DOMAIN = 'https://bigfungus.ca'
-@app.route('/create-checkout-session/', methods=['POST'])
-def create_checkout_session():
-    cart = request.form
-    print(cart)
-    line_items = [
-    ]
-    for item in cart:
-        line_items.append({
-                'price':item,
-                'quantity':cart[item]
-            })
-
-    checkout_session = stripe.checkout.Session.create(
-        shipping_address_collection={
-            'allowed_countries': ['CA'],
-        },
-        shipping_options=getShippingOptionsStripe(),
-        line_items=line_items,
-        mode='payment',
-        success_url= YOUR_DOMAIN + '/shop',
-        cancel_url= YOUR_DOMAIN + '/shop',
-    )
-    return redirect(checkout_session.url, code=303)
 
 if __name__ == '__main__':
     # context = ('cert.pem', 'key.pem') #certificate and key files  // ssl_context=context
--- content/seo.py
+++ content/seo.py
@@ -1,5 +1,5 @@
 meta = {
-        "Guides": {
+        "Grow Guides": {
             "Grow-Guides": {
                 "title": "Mushroom growing: Collective knowledge for growing mushrooms",
                 "description" : "Growing mushrooms can be tricky. BigFungus provides you with all the necessary information needed to grow mushrooms indoor and outdoors. Whether you are searching for beginner techniques, obscure teks or anything of the like, bigfungus provides you with vetted protocols and established experience to grow your very own mushrooms at home."
@@ -16,11 +16,15 @@ meta = {
                 "title": "Mushroom recipes: Unbiased recipe collection for all mushroom enthusiasts",
                 "description" : "Finding the perfect recipe to grow your produce is always challenging. This Tool gathers content from various cooking site and offers you with unbiased reviews."
             }
-        },
-        "Shop" : {
-            "title": "BigFungus: Online Mushroom growing supply store",
+            },
+        "Identification": {
+                "title": "Mushroom Identification: Easy to follow identification of mushrooms.",
+                "description" : "Identifying mushrooms can be tricky. This page is a beginner friendly, ready to use mushroom identification guide." 
+            },
+        "Products" : {
+            "title": "BigFungus: Providing you with top tier mushroom strains and types.",
             "description" : "BigFungus provides you with the highest quality, clean, and affordable mushroom growing products to start you very own experience at home."
-        }
+            },
     }
 
 def getMeta() -> dict:
